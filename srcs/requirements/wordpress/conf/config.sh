0aec016203589d8b208b9452722512655d477c23
Abderazzak Oumad <96908237+aoumad@users.noreply.github.com>
2023-07-10 18:48:44 +0100

    done

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -27,7 +27,7 @@ fi
 # Install Wordpress
 # This line uses the `wp` command to download the latest version of WordPress to the specified
 # path `($WP_PATH)`
-$WP_CLI core downlaod --path=$WP_PATH
+$WP_CLI core download --path=$WP_PATH
 
 
 # Create wp-config.php
@@ -35,9 +35,13 @@ $WP_CLI core downlaod --path=$WP_PATH
 # database credentials ($MYSQL_DATABASE, $MYSQL_USER, $MYSQL_PASSWORD, $DB_HOST) and other
 # configuration options. The `--force` flag forces overwriting the existing `wp-config.php`
 # file if it already exists
-$WP_CLI config create --dbname= $MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpaass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path=$WP_PATH --force
+$WP_CLI config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --path=$WP_PATH --force
 
-
+# Modify wp-config.php with database configuration
+sed -i -r "s/database_name_here/$MYSQL_DATABASE/1" $WP_PATH/wp-config.php
+sed -i -r "s/username_here/$MYSQL_USER/1" $WP_PATH/wp-config.php
+sed -i -r "s/password_here/$MYSQL_PASSWORD/1" $WP_PATH/wp-config.php
+sed -i -r "s/localhost/$DB_HOST/1" $WP_PATH/wp-config.php
 
 # Set file permissions and ownership
 # These lines set the file permissions (`chmod`) and ownership (`chown`) of the Wordpress
